bf798eae8ec1e0c04b4f1b6574bcb0eaf1369497
BAbdalwahed <b.abdalwahed@hotmail.com>
2022-11-13 19:08:41 +0000

    kata all done

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,5 @@
-const getEmployerRole = (employeeName, employees) => {};
-
+const getEmployerRole = (employeeName, employees) => {
+  let person = employees.filter((person) => person.name == employeeName);
+  return person[0].role;
+};
 module.exports = getEmployerRole;
